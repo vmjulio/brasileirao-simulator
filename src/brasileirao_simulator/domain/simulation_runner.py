fb5549935775056c76cd12ccb3c3b8afb2464844
Vitor <vijulio@gmail.com>
2024-11-02 16:39:58 -0300

    restate simulation_runner class

--- src/brasileirao_simulator/domain/simulation_runner.py
+++ src/brasileirao_simulator/domain/simulation_runner.py
@@ -1,37 +1,34 @@
 from typing import Any
-import pandas as pd
 
 
 class SimulationRunner:
-    def __init__(
-            self,
-            fixtures: pd.DataFrame,
-            remaining_games: pd.DataFrame,
-            simulator: Any,
-            logger: Any,
-            persistence: Any,
-            params: Any
-    ) -> None:
-        self.fixtures: pd.DataFrame = fixtures
-        self.remaining_games: pd.DataFrame = remaining_games
-        self.simulator: Any = simulator
-        self.logger: Any = logger
+    def __init__(self,
+                 fixtures: Any,
+                 remaining_games: Any,
+                 simulator: Any,
+                 logger: Any,
+                 persistence: Any,
+                 params: Any) -> None:
+        self.fixtures = fixtures
+        self.remaining_games = remaining_games
+        self.simulator = simulator
+        self.logger = logger
 
         self.strategy: str = params.strategy
         self.iterations: int = params.iterations
         self.batch_size: int = params.max_batch_size
         self.load_results: bool = params.load_results
 
-        self.persistence: Any = persistence
+        self.persistence = persistence
 
         if self.load_results:
             self.logger.load_results(self.persistence.load_results(self.strategy))
 
     def run(self) -> None:
-        remaining_iterations = self.iterations
+        remaining_iterations: int = self.iterations
 
         while remaining_iterations > 0:
-            current_batch_size = min(remaining_iterations, self.batch_size)
+            current_batch_size: int = min(remaining_iterations, self.batch_size)
             print(f"Running batch of size: {current_batch_size}; Remaining iterations: {remaining_iterations}")
 
             for _ in range(current_batch_size):
@@ -40,4 +37,9 @@ class SimulationRunner:
                 bolao_standings = self.simulator.get_bolao_standings(simulated_fixtures)
 
                 self.logger.log_brasileirao_results(bras_standings)
-                self.logger.log_bolao_results(bolao_sta
+                self.logger.log_bolao_results(bolao_standings)
+
+            self.persistence.save_results(self.logger.get_results(), self.strategy)
+            remaining_iterations -= current_batch_size
+
+        self.logger.print_results()
