00ed54885a0f823a881492b92ca531e84e118f9f
Vitor <vijulio@gmail.com>
2024-11-19 18:54:05 -0300

    allow as-of analysis in tables

--- src/brasileirao_simulator/domain/tables.py
+++ src/brasileirao_simulator/domain/tables.py
@@ -2,19 +2,28 @@ from brasileirao_simulator.domain.queries import Queries
 from brasileirao_simulator.domain.datasets import punters, doubles, fixtures
 import duckdb
 import pandas as pd
+import numpy as np
 
 
 class Tables:
     def __init__(self) -> None:
         self.con = duckdb.connect()
 
-    def enriched_tidy_fixtures(self) -> pd.DataFrame:
+    def enriched_tidy_fixtures(self, blank_from_date: str = None) -> pd.DataFrame:
         tidy_fixtures: pd.DataFrame = self.con.sql(Queries().tidy_fixtures()).df()
+        if blank_from_date:
+            condition = tidy_fixtures["fixture_date"] > (blank_from_date + " 23:59:59")
+            new_values = {
+                'goals_for': np.nan,
+                'goals_against': np.nan,
+                'points': 0.0
+            }
+            tidy_fixtures.loc[condition, ['goals_for', 'goals_against', 'points']] = tidy_fixtures.loc[condition, ['goals_for', 'goals_against', 'points']].assign(**new_values)
         return self.con.sql(Queries().enriched_tidy_fixtures()).df()
 
-    def remaining_games(self) -> pd.DataFrame:
-        enriched_tidy_fixtures: pd.DataFrame = self.enriched_tidy_fixtures()
+    def remaining_games(self, blank_from_date: str = None) -> pd.DataFrame:
+        enriched_tidy_fixtures: pd.DataFrame = self.enriched_tidy_fixtures(blank_from_date)
         home_filter = enriched_tidy_fixtures["venue"] == "home"
-        not_player_filter = enriched_tidy_fixtures["goals_for"].isnull()
+        not_played_filter = enriched_tidy_fixtures["goals_for"].isnull()
 
-        return enriched_tidy_fixtures[(not_player_filter) & (home_filter)]
+        return enriched_tidy_fixtures[(not_played_filter) & (home_filter)]
