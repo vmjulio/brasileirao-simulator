2e22ca40e14cf9d1ee87815efb28b38715a77fbd
Vitor <vijulio@gmail.com>
2024-11-19 18:44:27 -0300

    add entrypoint file

---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 brasileirao_simulator/entrypoints/backfill.py;
+python3 brasileirao_simulator/entrypoints/results_history_dataset.py;
+python3 brasileirao_simulator/entrypoints/matches_history_dataset.py;
