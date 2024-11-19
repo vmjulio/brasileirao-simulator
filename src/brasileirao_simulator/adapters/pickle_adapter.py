a7580cbf3cbe3b2386a709b86a5969b0cf9fe11c
Vitor <vijulio@gmail.com>
2024-11-19 18:55:05 -0300

    add suffix to pickle adapter

--- src/brasileirao_simulator/adapters/pickle_adapter.py
+++ src/brasileirao_simulator/adapters/pickle_adapter.py
@@ -8,12 +8,19 @@ class PickleAdapter(PersistencePort):
     def __init__(self, directory: str) -> None:
         self.directory: str = directory
 
-    def save_results(self, results: Any, strategy: str) -> None:
-        file_path = f"{self.directory}/{strategy}_results.pkl"
+    def save_results(self, results: Any, strategy: str, suffix: str = None) -> None:
+        appendix = ""
+        if suffix:
+            appendix = f"_{suffix}"
+        file_path = f"{self.directory}/{strategy}_results{appendix}.pkl"
+
         with open(file_path, "wb") as f:
             pickle.dump(results, f)
 
-    def load_results(self, strategy: str) -> Any:
-        file_path = f"{self.directory}/{strategy}_results.pkl"
+    def load_results(self, strategy: str, suffix: str = None) -> Any:
+        appendix = ""
+        if suffix:
+            appendix = f"_{suffix}"
+        file_path = f"{self.directory}/{strategy}_results{appendix}.pkl"
         with open(file_path, "rb") as f:
             return pickle.load(f)
