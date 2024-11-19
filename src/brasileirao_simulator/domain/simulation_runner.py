04e5cb2e27fc412380a0981271e1c714426690f2
Vitor <vijulio@gmail.com>
2024-11-19 18:54:34 -0300

    add suffix to simulation logs

--- src/brasileirao_simulator/domain/simulation_runner.py
+++ src/brasileirao_simulator/domain/simulation_runner.py
@@ -8,7 +8,8 @@ class SimulationRunner:
                  simulator: Any,
                  logger: Any,
                  persistence: Any,
-                 params: Any) -> None:
+                 params: Any,
+                 file_suffix: str = "") -> None:
         self.fixtures = fixtures
         self.remaining_games = remaining_games
         self.simulator = simulator
@@ -20,9 +21,10 @@ class SimulationRunner:
         self.load_results: bool = params.load_results
 
         self.persistence = persistence
+        self.file_suffix = file_suffix
 
         if self.load_results:
-            self.logger.load_results(self.persistence.load_results(self.strategy))
+            self.logger.load_results(self.persistence.load_results(strategy=self.strategy, suffix=self.file_suffix))
 
     def run(self) -> None:
         remaining_iterations: int = self.iterations
@@ -35,11 +37,14 @@ class SimulationRunner:
                 simulated_fixtures = self.simulator.simulate_fixtures(self.fixtures, self.remaining_games)
                 bras_standings = self.simulator.get_brasileirao_standings(simulated_fixtures)
                 bolao_standings = self.simulator.get_bolao_standings(simulated_fixtures)
+                match_results = self.simulator.get_match_results(simulated_fixtures)
 
                 self.logger.log_brasileirao_results(bras_standings)
                 self.logger.log_bolao_results(bolao_standings)
+                self.logger.log_match_results(match_results)
 
-            self.persistence.save_results(self.logger.get_results(), self.strategy)
+            self.persistence.save_results(results=self.logger.get_results(), strategy=self.strategy, suffix=self.file_suffix)
             remaining_iterations -= current_batch_size
 
         self.logger.print_results()
+        self.logger.print_matches()
