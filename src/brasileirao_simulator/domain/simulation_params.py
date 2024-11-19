3ddd1b27e0c664f3980f547cf35bc2dd8cc02966
Vitor <vijulio@gmail.com>
2024-11-19 18:53:45 -0300

    add params to file

--- src/brasileirao_simulator/domain/simulation_params.py
+++ src/brasileirao_simulator/domain/simulation_params.py
@@ -4,7 +4,8 @@ from typing import Optional
 
 @dataclass
 class SimulationParams:
-    iterations:int = 100
+    iterations:int = 500
     max_batch_size: int = 50
     load_results: bool = True
+    ignore_results_after: str = None
     strategy: Optional[str] = "average"
