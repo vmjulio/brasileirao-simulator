a2d36f7fa33695fa12b5715c4cbbba2832006439
Vitor <vijulio@gmail.com>
2024-11-19 18:52:29 -0300

    add match results log

--- src/brasileirao_simulator/domain/result_logger.py
+++ src/brasileirao_simulator/domain/result_logger.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import json
+from functools import partial
 from typing import Dict, Any
 
 
@@ -8,6 +9,7 @@ class ResultLogger:
         self.brasileirao_title_positions: Dict[str, int] = defaultdict(int)
         self.brasileirao_relegation_positions: Dict[str, int] = defaultdict(int)
         self.bolao_positions: Dict[str, int] = defaultdict(int)
+        self.match_results = defaultdict(partial(defaultdict, int))
 
     def log_brasileirao_results(self, bras_standings: Any) -> None:
         for row in bras_standings.to_dict(orient="records"):
@@ -21,23 +23,39 @@ class ResultLogger:
             if row["rank_"] == 1:
                 self.bolao_positions[row["punter"]] += 1
 
+    def log_match_results(self, match_results: Any) -> None:
+        for row in match_results.to_dict(orient="records"):
+            if row["home_goals"] > row["away_goals"]:
+                self.match_results[row["home_team"] + " x " + row["away_team"]]["home"] += 1
+            if row["home_goals"] < row["away_goals"]:
+                self.match_results[row["home_team"] + " x " + row["away_team"]]["away"] += 1
+            if row["home_goals"] == row["away_goals"]:
+                self.match_results[row["home_team"] + " x " + row["away_team"]]["draw"] += 1
+            self.match_results[row["home_team"] + " x " + row["away_team"]]["round_"] = row["round_"]
+
     def get_results(self) -> Dict[str, Dict[str, int]]:
         return {
             "brasileirao_title": self.brasileirao_title_positions,
             "brasileirao_relegation": self.brasileirao_relegation_positions,
-            "bolao": self.bolao_positions
+            "bolao": self.bolao_positions,
+            "match_results": self.match_results
         }
 
     def load_results(self, results: Dict[str, Dict[str, int]]) -> None:
         self.brasileirao_title_positions = results["brasileirao_title"]
         self.brasileirao_relegation_positions = results["brasileirao_relegation"]
         self.bolao_positions = results["bolao"]
+        self.match_results = results["match_results"]
 
     def _sorted_defaultdict(self, d: Dict[str, int], correction: float = 1.0) -> str:
         total = sum(d.values())
         r = dict(sorted(d.items(), key=lambda kv: kv[1], reverse=True))
         j = {team: f'{100 * count * correction / total:.2f}%' for team, count in r.items()}
         return json.dumps(j, indent=4)
+    
+    # TODO assign a type to d
+    def _sorted_nested_defaultdict(self, d, correction: float = 1.0) -> str:
+        return json.dumps(d, indent=4)
 
     def print_results(self) -> None:
         print("Brasileirao Title Positions:", self._sorted_defaultdict(self.brasileirao_title_positions))
@@ -45,3 +63,6 @@ class ResultLogger:
         print("Bolao Results:", self._sorted_defaultdict(self.bolao_positions))
         print("--")
         print("Total iterations:", sum([v for k, v in self.brasileirao_title_positions.items()]))
+    
+    def print_matches(self) -> None:
+        print("Matches:", self._sorted_nested_defaultdict(self.match_results))
